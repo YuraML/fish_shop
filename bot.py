9e0882e3f90a7c2ca32a8912ef4dbb58ea9cae5b
YuraML <malashtayuriy@gmail.com>
2023-08-06 18:53:11 +0400

    show detailed product

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler, CallbackContext
 
-from services import get_access_token, get_products
+from services import get_access_token, get_products, get_product
 
 _database = None
 
@@ -21,19 +21,26 @@ def start(update: Update, context: CallbackContext, access_token):
         keyboard[0].append(fish_button)
 
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
-    return "ECHO"
+    update.message.reply_text('Choose an available fish:', reply_markup=reply_markup)
+    return 'HANDLE_MENU'
 
 
-def echo(update: Update, context: CallbackContext, access_token):
-    users_reply = update.message.text
-    update.message.reply_text(users_reply)
-    return "ECHO"
+def handle_menu(update: Update, context: CallbackContext, access_token):
+    query = update.callback_query
+    product_id = query.data
+    product = get_product(access_token, product_id)['data']
+    product_name = product['attributes']['name']
+    product_description = product['attributes']['description']
+    text = f"{product_name}\n\n{product_description}"
+    context.bot.edit_message_text(text=text.format(query.data),
+                                  chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id)
+    return 'START'
 
 
 def handle_users_reply(update: Update, context: CallbackContext):
-    client_id = os.getenv("EP_CLIENT_ID")
-    client_secret = os.getenv("EP_CLIENT_SECRET")
+    client_id = os.getenv('EP_CLIENT_ID')
+    client_secret = os.getenv('EP_CLIENT_SECRET')
     access_token = get_access_token(client_id, client_secret)
     db = get_database_connection()
 
@@ -48,11 +55,11 @@ def handle_users_reply(update: Update, context: CallbackContext):
     if user_reply == '/start':
         user_state = 'START'
     else:
-        user_state = db.get(chat_id).decode("utf-8")
+        user_state = db.get(chat_id).decode('utf-8')
 
     states_functions = {
         'START': start,
-        'ECHO': echo
+        'HANDLE_MENU': handle_menu
     }
     state_handler = states_functions[user_state]
     try:
@@ -65,16 +72,16 @@ def handle_users_reply(update: Update, context: CallbackContext):
 def get_database_connection():
     global _database
     if _database is None:
-        database_password = os.getenv("DATABASE_PASSWORD")
-        database_host = os.getenv("DATABASE_HOST")
-        database_port = os.getenv("DATABASE_PORT")
+        database_password = os.getenv('DATABASE_PASSWORD')
+        database_host = os.getenv('DATABASE_HOST')
+        database_port = os.getenv('DATABASE_PORT')
         _database = redis.Redis(host=database_host, port=database_port, password=database_password)
     return _database
 
 
 if __name__ == '__main__':
     load_dotenv()
-    tg_token = os.getenv("TELEGRAM_TOKEN")
+    tg_token = os.getenv('TELEGRAM_TOKEN')
     updater = Updater(tg_token)
 
     dispatcher = updater.dispatcher
